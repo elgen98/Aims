1c5a8831607a117a1d9691d15297c7bd7cd2d6ac
elgen98 <elliot.blomqvist@yahoo.com>
2021-11-05 16:18:17 +0100

    input objects and lists with some styling

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,73 @@
-class Aims {
+/*class Aims {
   constructor(aimTitle, aimDescription) {
     this.title = aimTitle;
     this.description = aimDescription;
   }
-}
+}*/
 
 window.onload = function () {
-  createList();
+  //createList();
   document.getElementById("myBtn").addEventListener("click", showModal);
   document
     .getElementsByClassName("close")[0]
     .addEventListener("click", deleteModal);
+  document.getElementById("approveBtn").addEventListener("click", addObject);
 };
-let aim1 = new Aims("Träna", "Benböj");
-let aim2 = new Aims("Sova", "7 timmar");
-let aim3 = new Aims("Äta frukost", "Havregröt");
 
-function createList() {
-  let aimList = [aim1, aim2, aim3];
+let aimList = [];
+
+const title = document.getElementById("titleOfAim");
+const description = document.getElementById("descriptionOfAim");
+
+function createListItem() {
+  let last = aimList[aimList.length - 1];
+  let listContainer = document.createElement("div");
+  listContainer.className = "mainAim";
+
+  let ul = document.createElement("ul");
+  let ul2 = document.createElement("ul");
+  let li = document.createElement("li");
+  let li2 = document.createElement("li");
+
+  let check = document.createElement("input");
+  check.setAttribute("type", "checkbox");
+
+  li.innerHTML = last.title;
+  li2.innerHTML = last.description;
+
+  ul.appendChild(li);
+  listContainer.appendChild(check);
+  li.appendChild(ul2);
+  ul2.appendChild(li2);
+  listContainer.appendChild(ul);
+
+  document.getElementById("aims").appendChild(listContainer);
+}
+
+function addObject() {
+  aimList.push({
+    title: title.value,
+    description: description.value,
+  });
+  console.log(aimList);
+  createListItem();
+}
+
+function remove(position) {
+  aimList.splice(position, 1);
+}
+
+for (let i = 0; i < aimList.length; i++) {
+  let button = document.createElement("button");
+
+  button.addEventListener("click", () => {
+    remove(i);
+  });
+
+  button.appendChild(aimList[i]);
+}
+
+/*function createList() {
   let listContainer = document.createElement("div");
   listContainer.className = "mainAim";
 
@@ -36,7 +86,7 @@ function createList() {
     listContainer.appendChild(ul);
   }
   document.getElementById("aims").appendChild(listContainer);
-}
+}*/
 
 let modal = document.getElementById("myModal");
 
