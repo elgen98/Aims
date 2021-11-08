6f87a7bd95d7bdca93da870bb76891a4b8c48c8a
elgen98 <elliot.blomqvist@yahoo.com>
2021-11-08 18:54:05 +0100

    finished todos shown and divided in slides

--- JS/main.js
+++ JS/main.js
@@ -18,10 +18,14 @@ let aimList = [];
 const title = document.getElementById("titleOfAim");
 const description = document.getElementById("descriptionOfAim");
 let activeAim = true;
-document.getElementById("mainAims").innerHTML = "<h3>Dina Aims</h3>";
+document.getElementById("mainAims").innerHTML = "<h3>Your active Aims</h3>";
+document.getElementById("finishedAims").innerHTML =
+  "<h3>Your finished Aims</h3>";
 
 function createListItem() {
-  document.getElementById("mainAims").innerHTML = "<h3>Dina Aims</h3>";
+  document.getElementById("mainAims").innerHTML = "<h3>Your active Aims</h3>";
+  document.getElementById("finishedAims").innerHTML =
+    "<h3>Your finished Aims</h3>";
 
   for (let i = 0; i < aimList.length; i++) {
     if (aimList[i].activeAim == true) {
@@ -56,6 +60,38 @@ function createListItem() {
         ul.appendChild(li);
         document.getElementById("mainAims").appendChild(ul);
       }
+    } else {
+      let ul = document.createElement("ul");
+      let li = document.createElement("li");
+
+      let button = document.createElement("button");
+      button.className = "uncheckBtn";
+      button.innerHTML = '<i class="fas fa-check-circle"></i>';
+      button.addEventListener("click", () => {
+        add(i);
+      });
+
+      if (aimList[i].description.length > 0) {
+        let ul2 = document.createElement("ul");
+        let li2 = document.createElement("li");
+
+        li.innerHTML = aimList[i].title;
+        li2.innerHTML = aimList[i].description;
+
+        li.appendChild(button);
+
+        ul.appendChild(li);
+        li.appendChild(ul2);
+        ul2.appendChild(li2);
+        document.getElementById("finishedAims").appendChild(ul);
+      } else {
+        li.innerHTML = aimList[i].title;
+
+        li.appendChild(button);
+
+        ul.appendChild(li);
+        document.getElementById("finishedAims").appendChild(ul);
+      }
     }
   }
 }
@@ -72,6 +108,12 @@ function addObject() {
 
 function remove(position) {
   aimList[position].activeAim = false;
+  console.log(aimList);
+  createListItem();
+}
+
+function add(position) {
+  aimList[position].activeAim = true;
   console.log(aimList);
   createListItem();
 }
